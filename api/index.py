29a401a127315c9ee0aaa7dde51928c868a88eac
JJCE <jjce77@gmail.com>
2026-03-09 17:37:48 -0500

    Bien, el equirements.txt está correcto. Ahora sube el cambio en pi/index.py con el handler = Mangum(app) definido localmente y prueba el deploy.

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'apps', 'api'))
-from index import handler
+from index import app
+from mangum import Mangum
+
+handler = Mangum(app)
